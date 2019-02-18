699c9b4cffaf3693f4efeb7e1ed56df2cc7d0010
denmitchell <den.mitchell@yahoo.com>
2019-02-18 15:52:06 -0500

    ongoing work on test projects

--- EDennis.Samples.Hr.ExternalApi.Tests/CreateChecks/expected1.sql
+++ EDennis.Samples.Hr.ExternalApi.Tests/CreateChecks/expected1.sql
@@ -1,36 +1,50 @@
 ﻿use AgencyInvestigatorCheck;
 begin transaction
 declare @Id int = 1;
-declare @Input varchar(max) = (
-select 1 EmployeeId, '2018-12-01' DateCompleted, 'Pass' Status
-	for json path, without_array_wrapper
-);
-insert into AgencyInvestigatorCheck(EmployeeId,DateCompleted,Status) 
-	values (1,'2018-12-01','Pass');
+--declare @Input varchar(max) = (
+--select 1 EmployeeId, '2018-12-01' DateCompleted, 'Pass' Status
+--	for json path, without_array_wrapper
+--);
+--insert into AgencyInvestigatorCheck(EmployeeId,DateCompleted,Status) 
+--	values (1,'2018-12-01','Pass');
 declare @Expected varchar(max) = (
 	select 
+		a.DateCompleted as [AgencyInvestigatorCheck.DateCompleted],
+		a.Status as [AgencyInvestigatorCheck.Status]
+		from 
+		(select EmployeeId from AgencyInvestigatorCheck
+		union select EmployeeId from AgencyOnlineCheck
+		union select EmployeeId from FederalBackgroundCheck
+		union select EmployeeId from StateBackgroundCheck) emps
+		left outer join
 		(select top 1 DateCompleted, Status 
 			from AgencyInvestigatorCheck..AgencyInvestigatorCheck 
 			where EmployeeId = @Id
-			order by SysStart desc
-			for json path) AgencyInvestigatorCheck,
+			order by DateCompleted desc
+			for json path, without_array_wrapper) a
+		left outer join 
 		(select top 1 DateCompleted, Status 
 			from AgencyOnlineCheck..AgencyOnlineCheck
 			where EmployeeId = @Id
-			order by SysStart desc
-			for json path) AgencyOnlineCheck,
+			order by DateCompleted desc
+			for json path, without_array_wrapper) AgencyOnlineCheck,
 		(select top 1 DateCompleted, Status 
-			from FederalBackroundCheck..FederalBackroundCheck 
+			from FederalBackgroundCheck..FederalBackgroundCheck 
 			where EmployeeId = @Id
-			order by SysStart desc
-			for json path) FederalBackroundCheck,
+			order by DateCompleted desc
+			for json path, without_array_wrapper) FederalBackroundCheck,
 		(select top 1 DateCompleted, Status 
-			from StateBackroundCheck..StateBackroundCheck 
+			from StateBackgroundCheck..StateBackgroundCheck 
 			where EmployeeId = @Id
-			order by SysStart desc
-			for json path) StateBackroundCheck);
+			order by DateCompleted desc
+			for json path, without_array_wrapper) StateBackroundCheck
+		for json path);
+
+select @Expected
+
 rollback transaction
-exec _maintenance.ResetIdentities;
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Id',@Id
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Input',@Input
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Expected',@Expected
+--exec _maintenance.ResetIdentities;
+--exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Id',@Id
+--exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Input',@Input
+--exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.ExternalApi','EmployeeController','CreateChecks','MultitierIntegrationTests_InMemory','1','Expected',@Expected
+
