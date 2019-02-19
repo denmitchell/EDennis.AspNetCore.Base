bed146d5efc6afb09f2bf8ede1c3c2b5945ca48b
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2019-02-19 10:14:08 -0500

    ongoing work on test cases

--- EDennis.Samples.Hr.InternalApi1.Tests/TestCases/RepoTests/QueryPagedList/B.sql
+++ EDennis.Samples.Hr.InternalApi1.Tests/TestCases/RepoTests/QueryPagedList/B.sql
@@ -1,23 +1,24 @@
 ﻿use hr;
-declare @pageNumber int = 2
-declare @pageSize int = 1
-declare @alpha varchar(30) = 'o'
+declare @TestCase varchar(1) = 'B'
+declare @PageNumber int = 2
+declare @PageSize int = 1
+declare @Alpha varchar(30) = 'o'
 
-declare 
-	@expected varchar(max) = 
+declare @Expected varchar(max) = 
 (
 	select * from Employee
-	where FirstName like '%' + @alpha + '%'
-	order by id
-	offset @pageSize * (@pageNumber - 1) rows
-	fetch next @pageSize rows only 
-	for json path, include_null_values
+		where FirstName like '%' + @Alpha + '%'
+		order by Id
+		offset @PageSize * (@PageNumber - 1) rows
+		fetch next @PageSize rows only 
+		for json path, include_null_values
 );
 
 
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','B','Alpha', @alpha
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','B','PageNumber', @pageNumber
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','B','PageSize', @pageSize
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','B','Expected', @expected
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'Alpha', @Alpha
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'PageNumber', @PageNumber
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'PageSize', @PageSize
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'Expected', @Expected
 
---exec _maintenance.ResetIdentities
+exec  _maintenance.GetTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase
+
--- EDennis.Samples.Hr.InternalApi1.Tests/TestCases/RepoTests/QueryPagedList/C.sql
+++ EDennis.Samples.Hr.InternalApi1.Tests/TestCases/RepoTests/QueryPagedList/C.sql
@@ -1,23 +1,24 @@
 ﻿use hr;
-declare @pageNumber int = 1
-declare @pageSize int = 1
-declare @alpha varchar(30) = 'n'
+declare @TestCase varchar(1) = 'C'
+declare @PageNumber int = 1
+declare @PageSize int = 1
+declare @Alpha varchar(30) = 'n'
 
-declare 
-	@expected varchar(max) = 
+declare @Expected varchar(max) = 
 (
 	select * from Employee
-	where FirstName like '%' + @alpha + '%'
-	order by id
-	offset @pageSize * (@pageNumber - 1) rows
-	fetch next @pageSize rows only 
-	for json path, include_null_values
+		where FirstName like '%' + @alpha + '%'
+		order by id
+		offset @PageSize * (@PageNumber - 1) rows
+		fetch next @PageSize rows only 
+		for json path, include_null_values
 );
 
 
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','C','Alpha', @alpha
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','C','PageNumber', @pageNumber
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','C','PageSize', @pageSize
-exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'GetByLinq','GetByLinqPaging','C','Expected', @expected
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'Alpha', @Alpha
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'PageNumber', @PageNumber
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'PageSize', @PageSize
+exec _maintenance.SaveTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase,'Expected', @Expected
 
---exec _maintenance.ResetIdentities
+exec  _maintenance.GetTestJson 'EDennis.Samples.Hr.InternalApi1', 'EmployeeRepo', 'Query','PagedList',@TestCase
+
