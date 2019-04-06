ca9a4c0b0fa5b72d12fc705295a84ebce49633e8
denmitchell <den.mitchell@yahoo.com>
2019-04-06 03:53:10 -0400

    Removed several unused dependencies from the project

--- EDennis.AspNetCore.Base/EntityFramework/Abstractions/WriteableTemporalRepo.cs
+++ EDennis.AspNetCore.Base/EntityFramework/Abstractions/WriteableTemporalRepo.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Dynamic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
