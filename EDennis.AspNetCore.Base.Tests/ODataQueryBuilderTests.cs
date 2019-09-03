dae887d967e5b71d21dcf4ca3ed2fca119766dfb
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2019-09-03 16:52:50 -0400

    Added ODataQueryBuilder with FluentApi ... see EDennis.AspNetCore.Base.Tests.ODataQueryBuilderTests for sample usage.

---
+++ EDennis.AspNetCore.Base.Tests/ODataQueryBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using EDennis.AspNetCore.Base.Web;
+using EDennis.Samples.Colors.ExternalApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.AspNetCore.Base.Tests {
+    public class ODataQueryBuilderTests {
+
+        private readonly ITestOutputHelper _output;
+
+        public ODataQueryBuilderTests(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestBuilder1() {
+            var query = new ODataQueryBuilder()
+                .Select<Color>()
+                .Filter("name eq blue")
+                .OrderBy("name desc")
+                .Top(1)
+                .Skip(2)
+                .Query;
+            _output.WriteLine(query);
+        }
+
+
+
+    }
+}
---
+++ EDennis.AspNetCore.Base/Web/Extensions/ODataQueryBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDennis.AspNetCore.Base.Web {
+    public class ODataQueryBuilder {
+        public string Query { get; set; }
+        private char Operator {
+            get {
+                return (Query ?? "").Contains("?") ? '&' : '?';
+            }
+        }
+        public ODataQueryBuilder Select(params string[] properties) {
+            Query += $"{Operator}$select={string.Join(',', properties)}";
+            return this;
+        }
+        public ODataQueryBuilder Select<TEntity>(params string[] properties)
+            where TEntity : class {
+            var props = typeof(TEntity).GetProperties().Select(x=>x.Name).ToArray();
+            return Select(props);
+        }
+
+        public ODataQueryBuilder Filter(string filter) {
+            Query += $"{Operator}$filter={filter}";
+            return this;
+        }
+
+        public ODataQueryBuilder OrderBy(string orderBy) {
+            Query += $"{Operator}$orderBy={orderBy}";
+            return this;
+        }
+
+
+        public ODataQueryBuilder Top(int top) {
+            Query += $"{Operator}$top={top}";
+            return this;
+        }
+
+        public ODataQueryBuilder Skip(int skip) {
+            Query += $"{Operator}$skip={skip}";
+            return this;
+        }
+
+    }
+}
