5a36ad048e7dcacba75c43d99d0d4d06dff80ac4
Dennis Mitchell <den.mitchell@yahoo.com>
2019-10-18 05:03:38 -0400

    Updated AddTraceable to accommodate ApiClients

---
+++ EDennis.AspNetCore.Base/Web/Extensions/IServiceCollectionExtensions_Logging.cs
@@ -0,0 +1,111 @@
+﻿using Castle.DynamicProxy;
+using EDennis.AspNetCore.Base.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace EDennis.AspNetCore.Base.Web {
+    public static class IServiceCollectionExtensions_Logging {
+
+        public static IServiceCollection AddScopedTraceable<TInterface, TImplementation>(this IServiceCollection services)
+            where TInterface : class
+            where TImplementation : TInterface => services.AddTraceable<TInterface, TImplementation>(ServiceLifetime.Scoped);
+
+        public static IServiceCollection AddSingletonTraceable<TInterface, TImplementation>(this IServiceCollection services)
+            where TInterface : class
+            where TImplementation : TInterface => services.AddTraceable<TInterface, TImplementation>(ServiceLifetime.Singleton);
+
+        public static IServiceCollection AddTransientTraceable<TInterface, TImplementation>(this IServiceCollection services)
+            where TInterface : class
+            where TImplementation : TInterface => services.AddTraceable<TInterface, TImplementation>(ServiceLifetime.Transient);
+
+
+        public static IServiceCollection AddScopedTraceable<TImplementation>(this IServiceCollection services)
+            where TImplementation : class => services.AddTraceable<TImplementation, TImplementation>(ServiceLifetime.Scoped);
+
+        public static IServiceCollection AddSingletonTraceable<TImplementation>(this IServiceCollection services)
+            where TImplementation : class => services.AddTraceable<TImplementation, TImplementation>(ServiceLifetime.Singleton);
+
+        public static IServiceCollection AddTransientTraceable<TImplementation>(this IServiceCollection services)
+            where TImplementation : class => services.AddTraceable<TImplementation, TImplementation>(ServiceLifetime.Transient);
+
+
+        public static IServiceCollection AddTraceable<TInterface, TImplementation>(
+            this IServiceCollection services, ServiceLifetime serviceLifetime)
+            where TInterface : class
+            where TImplementation : TInterface {
+
+            var constructorParameters = typeof(TImplementation)
+                .GetConstructors()
+                .FirstOrDefault()
+                .GetParameters()
+                .Select(x => x.ParameterType)
+                .ToArray();
+
+            Type loggerType = constructorParameters
+                .FirstOrDefault(t => typeof(ILogger).IsAssignableFrom(t));
+            Type loggersType = GetIEnumerableType(loggerType);
+
+            object[] args = new object[constructorParameters.Count()];
+
+            if (constructorParameters.Any(t => t == typeof(HttpClient)))
+                services.AddHttpClient(); //adds all HttpClient-related services to collection;
+            
+            services.Add(new ServiceDescriptor(typeof(TInterface), f => {
+                var loggers = f.GetRequiredService(loggersType) as IEnumerable<object>;
+                var scopeProperties = f.GetRequiredService<ScopeProperties>();
+                ILogger activeLogger = (ILogger)loggers.ElementAt(scopeProperties.LoggerIndex);
+                for (int i = 0; i < args.Length; i++) {
+
+                    if (constructorParameters[i] == typeof(HttpClient)) {
+                        var httpClientFactory = f.GetRequiredService<IHttpClientFactory>();
+                        var httpClient = httpClientFactory.CreateClient(typeof(ApiClient).Name);
+                    }
+                    args[i] = f.GetRequiredService(constructorParameters[i]);
+                }
+                var proxy = (TImplementation)new ProxyGenerator()
+                    .CreateClassProxy(typeof(TImplementation), args,
+                        new TraceInterceptor(activeLogger, scopeProperties));
+
+                return proxy;
+            }, serviceLifetime));
+
+            return services;
+        }
+
+
+
+        public static IServiceCollection AddSecondaryLoggers(this IServiceCollection services,
+            params Type[] types) {
+
+            services.TryAddSingleton<ILoggerChooser>(f => {
+                var loggers = f.GetRequiredService<IEnumerable<ILogger<object>>>();
+                return new DefaultLoggerChooser(loggers);
+            });
+            for (int i = 0; i < types.Length; i++)
+                services.AddSingleton(typeof(ILogger<>), types[i]);
+
+            return services;
+        }
+
+
+
+        private static Type GetIEnumerableType<T>(T type)
+            where T : Type {
+
+            var iEnumerableType = typeof(IEnumerable<>);
+            var constructedIEnumerableType = iEnumerableType.MakeGenericType(type);
+
+            return constructedIEnumerableType;
+        }
+
+
+
+
+    }
+}
--- EDennis.AspNetCore.Base/Web/Extensions/IServiceCollectionExtensions_Repos.cs
+++ EDennis.AspNetCore.Base/Web/Extensions/IServiceCollectionExtensions_Repos.cs
@@ -1,181 +1,70 @@
-﻿using Castle.DynamicProxy;
-using EDennis.AspNetCore.Base.EntityFramework;
-using EDennis.AspNetCore.Base.Logging;
-using Microsoft.EntityFrameworkCore;
+﻿using EDennis.AspNetCore.Base.EntityFramework;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace EDennis.AspNetCore.Base.Web {
     public static class IServiceCollectionExtensions_Repos {
 
-
-        public static IServiceCollection AddRepo<TRepoImplementation, TContext>(this IServiceCollection services)
-            where TRepoImplementation : class, IRepo
-            where TContext : DbContext {
+        public static IServiceCollection AddRepos<TRepo>(this IServiceCollection services, bool traceable = true)
+            where TRepo : class, IRepo {
             services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepoImplementation, TContext>();
+            if (traceable)
+                services.AddScopedTraceable<TRepo>();
+            else
+                services.AddScoped<TRepo, TRepo>();
             return services;
         }
 
-
-
-        public static IServiceCollection AddRepo<TRepoInterface, TRepoImplementation, TContext>(this IServiceCollection services)
-            where TRepoInterface : class
-            where TRepoImplementation : class, IRepo, TRepoInterface
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepoInterface, TRepoImplementation, TContext>();
-            return services;
-        }
-
-        private static IServiceCollection AddScoped<TRepoImplementation, TContext>(this IServiceCollection services)
-            where TRepoImplementation : class
-            where TContext : DbContext {
-            return services.AddScoped(f => {
-                var loggers = f.GetRequiredService<IEnumerable<ILogger<TRepoImplementation>>>();
-                var scopeProperties = f.GetRequiredService<ScopeProperties>();
-                var activeLogger = loggers.ElementAt(scopeProperties.LoggerIndex);
-                var context = f.GetRequiredService<TContext>();
-                var repo = (TRepoImplementation)new ProxyGenerator()
-                    .CreateClassProxy(typeof(TRepoImplementation),
-                        new object[] { context, scopeProperties, activeLogger },
-                        new TraceInterceptor(activeLogger, scopeProperties));
-                return repo;
-            });
-        }
-
-        private static IServiceCollection AddScoped<TRepoInterface, TRepoImplementation, TContext>(this IServiceCollection services)
-            where TRepoInterface : class
-            where TRepoImplementation : class, TRepoInterface
-            where TContext : DbContext {
-            return services.AddScoped<TRepoInterface>(f => {
-                var loggers = f.GetRequiredService<IEnumerable<ILogger<TRepoInterface>>>();
-                var scopeProperties = f.GetRequiredService<ScopeProperties>();
-                var activeLogger = loggers.ElementAt(scopeProperties.LoggerIndex);
-                var context = f.GetRequiredService<TContext>();
-                var repo = (TRepoImplementation)new ProxyGenerator()
-                    .CreateClassProxy(typeof(TRepoImplementation),
-                        new object[] { context, scopeProperties, activeLogger },
-                        new TraceInterceptor(activeLogger, scopeProperties));
-                return repo;
-            });
-        }
-
-
-        private static IServiceCollection AddScoped<TRepoInterface, TRepoImplementation, TEntity, TContext, THistoryContext>(this IServiceCollection services)
-            where TRepoInterface : class
-            where TEntity : class, IEFCoreTemporalModel, new()
-            where TRepoImplementation : WriteableTemporalRepo<TEntity,TContext,THistoryContext>, TRepoInterface
-            where TContext : DbContext
-            where THistoryContext : DbContext {
-            return services.AddScoped<TRepoInterface>(f => {
-                var loggers = f.GetRequiredService<IEnumerable<ILogger<TRepoInterface>>>();
-                var scopeProperties = f.GetRequiredService<ScopeProperties>();
-                var activeLogger = loggers.ElementAt(scopeProperties.LoggerIndex);
-                var context = f.GetRequiredService<TContext>();
-                var histContext = f.GetRequiredService<THistoryContext>();
-                var repo = (TRepoImplementation)new ProxyGenerator()
-                    .CreateClassProxy(typeof(TRepoImplementation),
-                        new object[] { context, scopeProperties, activeLogger },
-                        new TraceInterceptor(activeLogger, scopeProperties));
-                return repo;
-            });
-        }
-
-        private static IServiceCollection AddScoped<TRepoImplementation, TEntity, TContext, THistoryContext>(this IServiceCollection services)
-            where TEntity : class, IEFCoreTemporalModel, new()
-            where TRepoImplementation : WriteableTemporalRepo<TEntity, TContext, THistoryContext>
-            where TContext : DbContext
-            where THistoryContext : DbContext {
-            return services.AddScoped(f => {
-                var loggers = f.GetRequiredService<IEnumerable<ILogger<TRepoImplementation>>>();
-                var scopeProperties = f.GetRequiredService<ScopeProperties>();
-                var activeLogger = loggers.ElementAt(scopeProperties.LoggerIndex);
-                var context = f.GetRequiredService<TContext>();
-                var histContext = f.GetRequiredService<THistoryContext>();
-                var repo = (TRepoImplementation)new ProxyGenerator()
-                    .CreateClassProxy(typeof(TRepoImplementation),
-                        new object[] { context, scopeProperties, activeLogger },
-                        new TraceInterceptor(activeLogger, scopeProperties));
-                return repo;
-            });
-        }
-
-        #region Obsolete
-
-        [Obsolete("Use AddRepo, instead")]
-        public static IServiceCollection AddRepos<TRepo1, TContext>(this IServiceCollection services)
+        public static IServiceCollection AddRepos<TRepo1, TRepo2>(this IServiceCollection services, bool traceable = true)
             where TRepo1 : class, IRepo
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepo1, TContext>();
+            where TRepo2 : class, IRepo {
+            services.AddRepos<TRepo1>();
+            if (traceable)
+                services.AddScopedTraceable<TRepo2>();
+            else
+                services.AddScoped<TRepo2, TRepo2>();
             return services;
         }
 
-        [Obsolete("Use AddRepo, instead")]
-        public static IServiceCollection AddRepos<TRepo1, TRepo2, TContext>(this IServiceCollection services)
+        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3>(this IServiceCollection services, bool traceable = true)
             where TRepo1 : class, IRepo
             where TRepo2 : class, IRepo
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepo1, TContext>();
-            services.AddScoped<TRepo2, TContext>();
+            where TRepo3 : class, IRepo {
+            services.AddRepos<TRepo1, TRepo2>();
+            if (traceable)
+                services.AddScopedTraceable<TRepo3>();
+            else
+                services.AddScoped<TRepo3, TRepo3>();
             return services;
         }
 
-        [Obsolete("Use AddRepo, instead")]
-        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3, TContext>(this IServiceCollection services)
+
+        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3, TRepo4>(this IServiceCollection services, bool traceable = true)
             where TRepo1 : class, IRepo
             where TRepo2 : class, IRepo
             where TRepo3 : class, IRepo
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepo1, TContext>();
-            services.AddScoped<TRepo2, TContext>();
-            services.AddScoped<TRepo3, TContext>();
+            where TRepo4 : class, IRepo {
+            services.AddRepos<TRepo1, TRepo2, TRepo3>();
+            if (traceable)
+                services.AddScopedTraceable<TRepo4>();
+            else
+                services.AddScoped<TRepo4, TRepo4>();
             return services;
         }
 
-
-        [Obsolete("Use AddRepo, instead")]
-        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3, TRepo4, TContext>(this IServiceCollection services)
+        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3, TRepo4, TRepo5>(this IServiceCollection services, bool traceable = true)
             where TRepo1 : class, IRepo
             where TRepo2 : class, IRepo
             where TRepo3 : class, IRepo
             where TRepo4 : class, IRepo
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepo1, TContext>();
-            services.AddScoped<TRepo2, TContext>();
-            services.AddScoped<TRepo3, TContext>();
-            services.AddScoped<TRepo3, TContext>();
-            services.AddScoped<TRepo4, TContext>();
+            where TRepo5 : class, IRepo {
+            services.AddRepos<TRepo1, TRepo2, TRepo3, TRepo4>();
+            if (traceable)
+                services.AddScopedTraceable<TRepo5>();
+            else
+                services.AddScoped<TRepo5, TRepo5>();
             return services;
         }
-
-        [Obsolete("Use AddRepo, instead")]
-        public static IServiceCollection AddRepos<TRepo1, TRepo2, TRepo3, TRepo4, TRepo5, TContext>(this IServiceCollection services)
-            where TRepo1 : class, IRepo
-            where TRepo2 : class, IRepo
-            where TRepo3 : class, IRepo
-            where TRepo4 : class, IRepo
-            where TRepo5 : class, IRepo
-            where TContext : DbContext {
-            services.TryAddScoped<ScopeProperties>();
-            services.AddScoped<TRepo1, TContext>();
-            services.AddScoped<TRepo2, TContext>();
-            services.AddScoped<TRepo3, TContext>();
-            services.AddScoped<TRepo3, TContext>();
-            services.AddScoped<TRepo4, TContext>();
-            services.AddScoped<TRepo5, TContext>();
-            return services;
-        }
-
-        #endregion
 
     }
 }
