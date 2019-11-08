4773831ccd42457b86b5d294476d935825c2ceb7
Dennis Mitchell <den.mitchell@yahoo.com>
2019-11-08 13:22:16 -0500

    Added RollbackController

---
+++ EDennis.AspNetCore.Base/Web/MiddlewareAndFilters/RollbackController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace EDennis.AspNetCore.Base.Web {
+
+    [Route("api/[controller]")]//controller route
+    [ApiController]
+    public class RollbackController : ControllerBase {
+
+        [HttpPost("{user}")]
+        public async void Index([FromRoute] string user) {
+        }
+
+    }
+}
