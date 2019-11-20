e0021a77e845a0b88e3a2c3b9652b1ee67e310a6
Dennis Mitchell <den.mitchell@yahoo.com>
2019-11-19 19:32:26 -0500

    rework structure of repository into src, test, and doc folders, where the test folder contains multiple test solutions

---
+++ test/EDennis.AspNetCore.RepoTests/EDennis.AspNetCore.RepoTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace EDennis.AspNetCore.RepoTests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
---
+++ test/EDennis.AspNetCore.SecurityTests/EDennis.AspNetCore.SecurityTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace EDennis.AspNetCore.SecurityTests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
