7ddac80f3ddfb0ff89e5e65ef428b0d619bd87cb
Dennis Mitchell <den.mitchell@yahoo.com>
2019-11-28 05:59:08 -0500

    Created FactoryEndpointTests and LauncherEndpointTests

---
+++ src/EDennis.AspNetCore.Base/Testing/XunitBase/Abstractions/EndpointTests/FactoryEndpointTests.cs
@@ -0,0 +1,43 @@
+﻿using EDennis.AspNetCore.Base.Web;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.AspNetCore.Base.Testing {
+
+    public abstract class FactoryEndpointTests<TTestApis>: IClassFixture<TTestApis> 
+        where TTestApis : TestApisBase {
+
+        public abstract string ApiKey { get; }
+        public virtual string InstanceName { get; } = Guid.NewGuid().ToString();
+
+        public ITestOutputHelper Output { get; }
+
+        public TTestApis TestApis { get; }
+        public HttpClient HttpClient { get; }
+
+        public FactoryEndpointTests(
+            TTestApis testApis,
+            ITestOutputHelper output) {
+            TestApis = testApis;
+            Output = output;
+            HttpClient = testApis.CreateClient[ApiKey]();
+        }
+
+
+
+        public void Dispose() {
+            SendResetAsync().Wait();
+        }
+
+        private async Task SendResetAsync() {
+            var clients = TestApis.CreateClient.Values.ToList().Select(f => f.Invoke()).ToArray();
+            await MiddlewareUtils.Reset(clients, TestApis.InstanceName);
+        }
+
+
+    }
+}
---
+++ src/EDennis.AspNetCore.Base/Testing/XunitBase/Abstractions/EndpointTests/LauncherEndpointTests.cs
@@ -0,0 +1,37 @@
+﻿using EDennis.AspNetCore.Base.Web;
+using Microsoft.Extensions.Configuration;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.AspNetCore.Base.Testing {
+
+    public class LauncherEndpointTests<TProgram,TLauncher> 
+        : IClassFixture<LauncherFixture<TProgram, TLauncher>>,
+        IDisposable
+
+        where TProgram : IProgram, new()
+        where TLauncher : ILauncher, new()
+        {
+
+        protected ITestOutputHelper Output { get; }
+        protected LauncherFixture<TProgram, TLauncher> LauncherFixture { get; }
+        public virtual ProgramBase Program { get; set; }
+        protected Apis Apis { get; }
+        public virtual string InstanceName { get; } = Guid.NewGuid().ToString();
+
+        public LauncherEndpointTests(ITestOutputHelper output,
+            LauncherFixture<TProgram, TLauncher> launcherFixture) {
+            Output = output;
+            LauncherFixture = launcherFixture;
+            Apis = new Apis();
+            Program.Configuration.GetSection(Program.ApisConfigurationSection).Bind(Apis);
+        }
+
+
+        public void Dispose() {
+            MiddlewareUtils.Reset(Apis,InstanceName);
+        }
+
+    }
+}
