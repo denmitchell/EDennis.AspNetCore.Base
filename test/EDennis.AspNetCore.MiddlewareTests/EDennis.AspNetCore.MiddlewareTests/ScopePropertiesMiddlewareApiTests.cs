90818c165e3b7fe5139f93a75d8fb35929a5e260
Dennis Mitchell <den.mitchell@yahoo.com>
2019-12-18 05:55:21 -0500

    ongoing testing work ... ongoing work with ScopePropertiesMiddlewareApiTests

---
+++ test/EDennis.AspNetCore.MiddlewareTests/EDennis.AspNetCore.MiddlewareTests/ScopePropertiesMiddlewareApiTests.cs
@@ -0,0 +1,51 @@
+﻿using EDennis.AspNetCore.Base;
+using EDennis.AspNetCore.Base.Web;
+using EDennis.Samples.ScopePropertiesMiddlewareApi.Tests;
+using System.Net.Http;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.AspNetCore.MiddlewareTests {
+    [Collection("Sequential")]
+    public class ScopePropertiesMiddlewareApiTests :
+        IClassFixture<TestApis> {
+
+
+        private readonly TestApis _factory;
+        private HttpClient _client;
+
+        private readonly ITestOutputHelper _output;
+        public ScopePropertiesMiddlewareApiTests(
+            TestApis factory,
+            ITestOutputHelper output) {
+            _factory = factory;
+            _output = output;
+
+            _client = _factory.CreateClient["ScopePropertiesMiddlewareApi"]();
+
+        }
+
+
+        //[Theory]
+        //[InlineData(0)]
+        //[InlineData(1)]
+        //public void TestScopeProperties(int testCase) {
+
+        //    var result = _client.Get<ScopeProperties>($"ScopeProperties");
+        //    ScopeProperties obj = (ScopeProperties)result.Value;
+
+        //    var json = JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+        //    _output.WriteLine(json);
+
+
+        //    Assert.Equal(UserSource.JwtSubjectClaim, obj.UserSource);
+        //    Assert.True(obj.AppendHostPath);
+        //    Assert.True(obj.CopyClaims);
+        //    Assert.True(obj.CopyHeaders);
+
+        //}
+
+
+    }
+}
--- test/EDennis.AspNetCore.MiddlewareTests/EDennis.Samples.ScopePropertiesMiddlewareApi.Lib/Startup.cs
+++ test/EDennis.AspNetCore.MiddlewareTests/EDennis.Samples.ScopePropertiesMiddlewareApi.Lib/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EDennis.AspNetCore.Base;
+using EDennis.AspNetCore.Base.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Linq;
 
 namespace EDennis.Samples.ScopePropertiesMiddlewareApi.Lib {
     public class Startup {
@@ -22,6 +20,10 @@ namespace EDennis.Samples.ScopePropertiesMiddlewareApi.Lib {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+
+            //configure ScopePropertiesMiddleware
+            var _ = new ServiceConfig(services, Configuration)
+                .AddScopeProperties();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +37,33 @@ namespace EDennis.Samples.ScopePropertiesMiddlewareApi.Lib {
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //for testing purposes:
+            //intercept request to add claims and headers 
+            //(for ScopePropertiesMiddleware to process)
+            app.Use(async (context, next) => {
+
+                //add claims from query string
+                var claims = new List<Claim>();
+                foreach (var query in context.Request.Query.Where(q=>q.Key.StartsWith("claim*")))
+                    claims.Add(new Claim(query.Key.Substring("claim*".Length), query.Value));
+
+                if (claims.Count > 0) {
+                    var appIdentity = new ClaimsIdentity(claims);
+                    context.User.AddIdentity(appIdentity);
+                }
+
+                //add headers from query string
+                foreach (var query in context.Request.Query.Where(q => q.Key.StartsWith("header*")))
+                    context.Request.Headers.Add(query.Key.Substring("header*".Length), query.Value);
+
+                await next();
+
+            });
+
+            //Use ScopePropertiesMiddleware
+            app.UseScopeProperties();
+
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
