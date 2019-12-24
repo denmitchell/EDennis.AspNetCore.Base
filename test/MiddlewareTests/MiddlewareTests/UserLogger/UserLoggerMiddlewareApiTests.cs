a49e409a11943798783127966f45cd9b0157e40d
Dennis Mitchell <den.mitchell@yahoo.com>
2019-12-24 04:01:09 -0500

    ongoing testing work ... UserLoggerMiddleware

---
+++ test/MiddlewareTests/MiddlewareTests/UserLogger/UserLoggerMiddlewareApiTests.cs
@@ -0,0 +1,82 @@
+﻿using EDennis.AspNetCore.Base.Web;
+using EDennis.NetCoreTestingUtilities;
+using EDennis.NetCoreTestingUtilities.Extensions;
+using EDennis.Samples.UserLoggerMiddlewareApi.Tests;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EDennis.AspNetCore.MiddlewareTests {
+    /// <summary>
+    /// Note: IClassFixture was not used because
+    /// the individual test cases were conflicting with each
+    /// (possibly, one test case was updating the configuration
+    /// while another test case was calling Get).  To resolve
+    /// the issue, I instantiate the TestApis within the
+    /// test method.  This is inefficient, but it works.
+    /// </summary>
+    [Collection("Sequential")]
+    public class UserLoggerMiddlewareApiTests {
+
+        private readonly ITestOutputHelper _output;
+
+        public UserLoggerMiddlewareApiTests(
+            ITestOutputHelper output) {
+            _output = output;
+        }
+
+
+        internal class TestJsonA : TestJsonAttribute {
+            public TestJsonA(string methodName, string testScenario, string testCase)
+                : base("UserLoggerApi", "UserLoggerController",
+                      methodName, testScenario, testCase, NetCoreTestingUtilities.DatabaseProvider.Excel, "UserLogger\\TestJson.xlsx") {
+            }
+        }
+
+
+        [Theory]
+        [TestJsonA("Get", "", "A")]
+        [TestJsonA("Get", "", "B")]
+        [TestJsonA("Get", "", "C")]
+        public void Get(string t, JsonTestCase jsonTestCase) {
+
+            using var factory = new TestApis();
+            var client = factory.CreateClient["UserLoggerApi"]();
+            _output.WriteLine($"Test case: {t}");
+
+            //send configuration for test case
+            var jcfg = File.ReadAllText($"UserLogger\\{jsonTestCase.TestCase}.json");
+            var status = client.Configure("", jcfg);
+
+            //make sure that configuration was successful
+            Assert.Equal((int)System.Net.HttpStatusCode.OK, status.GetStatusCode());
+
+            TestUrl(jsonTestCase, client, 1);
+            TestUrl(jsonTestCase, client, 2);
+            TestUrl(jsonTestCase, client, 3);
+
+        }
+
+
+        private void TestUrl(JsonTestCase jsonTestCase, HttpClient client, int index) {
+
+            var qry = jsonTestCase.GetObject<string>($"QueryString{index}");
+            var expected = jsonTestCase.GetObject<string>($"Expected{index}");
+
+            var url = $"UserLogger?{qry}";
+
+            var result = client.GetAsync(client.BaseAddress.ToString() + url).Result;
+            var content = result.Content.ReadAsStringAsync().Result;
+            _output.WriteLine(content);
+
+            var actual = JsonSerializer.Deserialize<List<KeyValuePair<string, string>>>(content);
+
+            Assert.True(actual.IsEqualOrWrite(expected, _output, true));
+
+        }
+
+    }
+}
