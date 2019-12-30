df8fdd0dc795d7d65a14224452ea43588cc4ebfd
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2019-12-30 17:49:16 -0500

    Exploring ways to simply DbContext constructor

---
+++ src/EDennis.AspNetCore.Base/EntityFramework/Abstractions/ResettableDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace EDennis.AspNetCore.Base.EntityFramework.Abstractions {
+    public class ResettableDbContext : DbContext {
+
+            public ResettableDbContext(DbContextOptionsProvider<ResettableDbContext> provider) :
+                base(provider.DbContextOptions) {
+                if (!Database.IsInMemory() && provider.DisableAutoTransactions) {
+                    Database.AutoTransactionsEnabled = false;
+                    Database.UseTransaction(provider.Transaction as DbTransaction);
+                }
+            }
+
+        }
+    }
