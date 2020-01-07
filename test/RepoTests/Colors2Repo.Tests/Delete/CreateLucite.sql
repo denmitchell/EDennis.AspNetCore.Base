ce2d7b0b2cbdca6158fc34727a00e7da6c644700
Dennis Mitchell <den.mitchell@yahoo.com>
2020-01-07 05:11:30 -0500

    Initial setup of Colors2Repo.Tests (for testing a SqlRepo

---
+++ test/RepoTests/Colors2Repo.Tests/Delete/CreateLucite.sql
@@ -0,0 +1,36 @@
+﻿use Color2Db;
+declare @TestCase varchar(255) = 'Lucite'
+declare @Red int = 125
+declare @Green int = 208
+declare @Blue int = 182
+declare @ReadSeedMax int = -999142
+declare @WriteSeedMax int = -999146
+
+begin transaction
+insert into Rgb (Id, Name, Red, Green, Blue, SysUser, DateAdded) 
+    values 
+        (@WriteSeedMax,'WhiteSmoke', 245, 245, 245, 'jill@hill.org', CAST('2019-01-24T12:29:50.0000000' AS DateTime2)),
+        (@WriteSeedMax-1,'Yellow', 255, 255, 0, 'jack@hill.org', CAST('2019-01-23T12:29:50.0000000' AS DateTime2)),
+        (@WriteSeedMax-2,'YellowGreen', 154, 205, 50, 'jack@hill.org', CAST('2019-01-22T12:29:50.0000000' AS DateTime2)),
+        -- target
+        (@WriteSeedMax-3,@TestCase, @Red, @Green, @Blue, 'testuser@example.org', GETDATE())
+
+declare @Input varchar(max) = 
+(
+	select * from Rgb where Id = @WriteSeedMax-3
+	for json path, without_array_wrapper
+)
+
+declare @Expected varchar(max) = 
+(
+	select * from Rgb where Id <= @ReadSeedMax
+	for json path
+);
+
+rollback transaction
+--exec _.ResetSequences
+
+exec _.SaveTestJson 'Colors2Repo', 'RgbRepo', 'Create', '', @TestCase, 'Input', @Input
+exec _.SaveTestJson 'Colors2Repo', 'RgbRepo', 'Create', '', @TestCase, 'Expected', @Expected
+
+exec _.GetTestJson 'Colors2Repo', 'RgbRepo', 'Create', '', @TestCase
---
+++ test/RepoTests/Colors2Repo.Tests/RgbRepoTests.cs
@@ -0,0 +1,109 @@
+﻿using Colors2.Models;
+using EDennis.AspNetCore.Base.EntityFramework;
+using EDennis.AspNetCore.Base.Testing;
+using EDennis.NetCoreTestingUtilities;
+using EDennis.NetCoreTestingUtilities.Extensions;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using System.Linq;
+
+namespace RepoTests {
+    public class RgbRepoTests
+        : RepoTests<RgbRepo, Rgb, Color2DbContext>{
+        
+        public RgbRepoTests(ITestOutputHelper output) 
+            : base(output) { }
+
+        protected string[] PropertiesToIgnore { get; }
+            = new string[] { "SysStart", "SysEnd" };
+
+        internal class TestJsonA : TestJsonAttribute {
+            public TestJsonA(string methodName, string testScenario, string testCase)
+                : base("Colors2Repo", "RgbRepo",
+                      methodName, testScenario, testCase, DatabaseProvider.Excel, "TestJson.xlsx") {
+            }
+        }
+
+        private IEnumerable<Rgb> BaseExpected(JsonTestCase jsonTestCase) {
+            var readSeed = jsonTestCase.GetObject<List<Rgb>>($"ReadSeed");
+            var writeSeed = jsonTestCase.GetObject<List<Rgb>>($"WriteSeed");
+            return readSeed.Union(writeSeed);
+        }
+
+        private void WriteSeed(JsonTestCase jsonTestCase) {
+            var inputs = jsonTestCase.GetObject<List<Rgb>>($"WriteSeed");
+            foreach (var input in inputs)
+                Repo.Create(input);
+        }
+
+        [Fact]
+        public void GetTestJsonForProject() {
+            var attr = new TestJsonAttribute("Color2Db", "ColorsRepo", "RgbRepo", "Create", "", "A");
+            var cases = attr.GetData(null);
+            Assert.NotNull(cases);
+        }
+
+
+        [Theory]
+        [TestJsonA("Create", "", "A")]
+        [TestJsonA("Create", "", "B")]
+        public void Create(string t, JsonTestCase jsonTestCase) {
+
+            Output.WriteLine($"Test case: {t}");
+
+            WriteSeed(jsonTestCase);
+
+            var input = jsonTestCase.GetObject<Rgb>("Input");
+            var expected = jsonTestCase.GetObject<List<Rgb>>("Expected");
+
+            Repo.Create(input);
+
+            var actual = Repo.Query.ToPagedList();
+
+            Assert.True(actual.IsEqualAndWrite(expected,3,PropertiesToIgnore,Output,true));
+        }
+
+
+        [Theory]
+        [TestJsonA("Update", "", "A")]
+        [TestJsonA("Update", "", "B")]
+        public void Update(string t, JsonTestCase jsonTestCase) {
+
+            Output.WriteLine($"Test case: {t}");
+
+            WriteSeed(jsonTestCase);
+
+            var input = jsonTestCase.GetObject<Rgb>("Input");
+            var id = jsonTestCase.GetObject<int>("Id");
+            var expected = jsonTestCase.GetObject<List<Rgb>>("Expected");
+
+            Repo.Update(input,id);
+
+            var actual = Repo.Query.ToPagedList();
+
+            Assert.True(actual.IsEqualAndWrite(expected, 3, PropertiesToIgnore, Output, true));
+        }
+
+        [Theory]
+        [TestJsonA("Delete", "", "A")]
+        [TestJsonA("Delete", "", "B")]
+        public void Delete(string t, JsonTestCase jsonTestCase) {
+
+            Output.WriteLine($"Test case: {t}");
+
+            WriteSeed(jsonTestCase);
+
+            var id = jsonTestCase.GetObject<int>("Id");
+            var expected = jsonTestCase.GetObject<List<Rgb>>("Expected");
+
+            Repo.Delete(id);
+
+            var actual = Repo.Query.ToPagedList();
+
+            Assert.True(actual.IsEqualAndWrite(expected, 3, PropertiesToIgnore, Output, true));
+        }
+
+
+    }
+}
