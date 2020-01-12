a83a41cec7f2c48333ee8800c1480cafdbc5e138
Dennis Mitchell <den.mitchell@yahoo.com>
2020-01-12 06:17:41 -0500

    ongoing api testing ... initial work on ReadonlyControllerTests ... GetDevExtreme, GetDynamicLinq, and GetDynamicLinqAsync now passing.

--- src/EDennis.AspNetCore.Base/Testing/XunitBase/ControllerTests/SqlServerReadonlyControllerTests.cs
+++ src/EDennis.AspNetCore.Base/Testing/XunitBase/ControllerTests/SqlServerReadonlyControllerTests.cs
@@ -12,6 +12,9 @@ using Xunit.Abstractions;
 using DevExtreme.AspNet.Data.ResponseModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using EDennis.NetCoreTestingUtilities.Extensions;
+using System.Threading.Tasks;
+using EDennis.NetCoreTestingUtilities;
 
 namespace EDennis.AspNetCore.Base.Testing {
     public abstract class SqlServerReadonlyControllerTests<TController, TRepo, TEntity, TContext> :
@@ -28,6 +31,19 @@ namespace EDennis.AspNetCore.Base.Testing {
         }
 
 
+        /// <summary>
+        /// Use this method for testing if you want full control over
+        /// entry of the parameters to the controller method.
+        /// </summary>
+        /// <param name="select"></param>
+        /// <param name="sort"></param>
+        /// <param name="filter"></param>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <param name="totalSummary"></param>
+        /// <param name="group"></param>
+        /// <param name="groupSummary"></param>
+        /// <returns></returns>
         public List<dynamic> GetDevExtremeResult(string select, string sort, string filter, int skip, int take, string totalSummary, string group, string groupSummary) {
             var iar = Controller.GetDevExtreme(select, sort, filter, skip, take, totalSummary, group, groupSummary);
             var data = ((LoadResult)(iar as ObjectResult).Value).data;
@@ -39,6 +55,126 @@ namespace EDennis.AspNetCore.Base.Testing {
             }
             return ToGeneric().ToList();
         }
+
+
+        /// <summary>
+        /// Returns actual and expected results from GetDevExtreme.
+        /// Note: this method looks for the following optional TestJson
+        /// parameters (case sensitive):
+        /// Select, Filter, Sort, Skip, and Take
+        /// </summary>
+        /// <param name="jsonTestCase"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public ExpectedActualList<Dictionary<string, object>> GetDevExtreme_ExpectedActual(string t, JsonTestCase jsonTestCase) {
+            Output.WriteLine(t);
+            var select = jsonTestCase.GetObjectOrDefault<string>("Select", Output);
+            var filter = jsonTestCase.GetObjectOrDefault<string>("Filter", Output);
+            var sort = jsonTestCase.GetObjectOrDefault<string>("Sort", Output);
+            var skip = jsonTestCase.GetObjectOrDefault<int>("Skip", Output);
+            var take = jsonTestCase.GetObjectOrDefault<int>("Take", Output);
+            if (take == default)
+                take = int.MaxValue;
+
+            var expectedDynamic = jsonTestCase.GetObject<List<dynamic>>("Expected");
+            var expected = ObjectExtensions.ToPropertyDictionaryList(expectedDynamic);
+
+            var actualDynamic = GetDevExtremeResult(select, sort, filter, skip, take, null, null, null);
+            var actual = ObjectExtensions.ToPropertyDictionaryList(actualDynamic);
+
+            return new ExpectedActualList<Dictionary<string, object>> { Expected = expected, Actual = actual };
+        }
+
+
+        /// <summary>
+        /// Use this method for testing if you want full control over
+        /// entry of the parameters to the controller method.
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="select"></param>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
+        public List<dynamic> GetDynamicLinqResult(string where, string orderBy, string select, int? skip, int? take) {
+            var iar = Controller.GetDynamicLinq(where, orderBy, select, skip, take);
+            var data = ((List<dynamic>)(iar as ObjectResult).Value);
+            return data;
+        }
+
+
+        /// <summary>
+        /// Returns actual and expected results from GetDynamicLinq.
+        /// Note: this method looks for the following optional TestJson
+        /// parameters (case sensitive):
+        /// Where, OrderBy, Select, Skip, and Take
+        /// </summary>
+        /// <param name="jsonTestCase"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public ExpectedActualList<Dictionary<string, object>> GetDynamicLinq_ExpectedActual(string t, JsonTestCase jsonTestCase) {
+            Output.WriteLine(t);
+            var where = jsonTestCase.GetObjectOrDefault<string>("Where", Output);
+            var orderBy = jsonTestCase.GetObjectOrDefault<string>("OrderBy", Output);
+            var select = jsonTestCase.GetObjectOrDefault<string>("Select", Output);
+            var skip = jsonTestCase.GetObjectOrDefault<int?>("Skip", Output);
+            var take = jsonTestCase.GetObjectOrDefault<int?>("Take", Output);
+
+            var expectedDynamic = jsonTestCase.GetObject<List<dynamic>>("Expected");
+            var expected = ObjectExtensions.ToPropertyDictionaryList(expectedDynamic);
+
+            var actualDynamic = GetDynamicLinqResult(where, orderBy, select, skip, take);
+            var actual = ObjectExtensions.ToPropertyDictionaryList(actualDynamic);
+
+            return new ExpectedActualList<Dictionary<string, object>> { Expected = expected, Actual = actual };
+        }
+
+
+        /// <summary>
+        /// Use this method for testing if you want full control over
+        /// entry of the parameters to the controller method.
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="select"></param>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
+        public async Task<List<dynamic>> GetDynamicLinqAsyncResult(string where, string orderBy, string select, int? skip, int? take) {
+            var iar = await Controller.GetDynamicLinqAsync(where, orderBy, select, skip, take);
+            var data = ((List<dynamic>)(iar as ObjectResult).Value);
+            return data;
+        }
+
+
+        /// <summary>
+        /// Returns actual and expected results from GetDynamicLinq.
+        /// Note: this method looks for the following optional TestJson
+        /// parameters (case sensitive):
+        /// Where, OrderBy, Select, Skip, and Take
+        /// </summary>
+        /// <param name="jsonTestCase"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public async Task<ExpectedActualList<Dictionary<string, object>>> GetDynamicLinqAsync_ExpectedActual(string t, JsonTestCase jsonTestCase) {
+
+            Output.WriteLine(t);
+
+            var where = jsonTestCase.GetObjectOrDefault<string>("Where", Output);
+            var orderBy = jsonTestCase.GetObjectOrDefault<string>("OrderBy", Output);
+            var select = jsonTestCase.GetObjectOrDefault<string>("Select", Output);
+            var skip = jsonTestCase.GetObjectOrDefault<int?>("Skip", Output);
+            var take = jsonTestCase.GetObjectOrDefault<int?>("Take", Output);
+
+            var expectedDynamic = jsonTestCase.GetObject<List<dynamic>>("Expected");
+            var expected = ObjectExtensions.ToPropertyDictionaryList(expectedDynamic);
+
+            var actualDynamic = await GetDynamicLinqAsyncResult(where, orderBy, select, skip, take);
+            var actual = ObjectExtensions.ToPropertyDictionaryList(actualDynamic);
+
+            return new ExpectedActualList<Dictionary<string, object>> { Expected = expected, Actual = actual };
+        }
+
 
     }
 }
--- test/ApiTests/Colors2Api.ControllerTests/ReadonlyControllerTests.cs
+++ test/ApiTests/Colors2Api.ControllerTests/ReadonlyControllerTests.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,197 +20,43 @@ namespace EDennis.Samples.Colors2Api.Tests
         public ReadonlyControllerTests(ITestOutputHelper output) : base(output) {
         }
 
+        public readonly string[] propsToIgnore = new string[] { "SysStart", "SysEnd" };
 
         internal class TestJson_ : TestJsonAttribute {
             public TestJson_(string methodName, string testScenario, string testCase)
-                : base("Colors2Db", "Colors2Api", "HslController", methodName, testScenario, testCase) {
+                : base("Color2Db", "Colors2Api", "HslController", methodName, testScenario, testCase) {
             }
         }
 
 
+        //NOTE: OData is endpoint testable only
 
 
-        //[Theory]
-        //[TestJson_("GetOData", "FilterSkipTop", "HueGt200Skip2Top5")]
-        //public void GetOData1(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var filter = jsonTestCase.GetObject<string>("Filter");
-        //    var skip = jsonTestCase.GetObject<int>("Skip");
-        //    var top = jsonTestCase.GetObject<int>("Top");
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/odata?$filter={filter}&$skip={skip}&$top={top}")
-        //        .GetObject<List<dynamic>>()
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-        //}
-
-        //[Theory]
-        //[TestJson_("GetOData", "FilterOrderBySelectTop", "NameContainsBlueSelectNameDescSysUserTop10")]
-        //public void GetOData2(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var filter = jsonTestCase.GetObject<string>("Filter");
-        //    var select = jsonTestCase.GetObject<string>("Select");
-        //    var orderBy = jsonTestCase.GetObject<string>("OrderBy");
-        //    var top = jsonTestCase.GetObject<int>("Top");
-
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderByDescending(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/odata?$filter={filter}&$select={select}&$orderBy={orderBy}&$top={top}")
-        //        .GetObject<List<dynamic>>()
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-
-        //}
+        [Theory]
+        [TestJson_("GetDevExtreme", "ReadonlyControllerTests|FilterSkipTake", "A")]
+        [TestJson_("GetDevExtreme", "ReadonlyControllerTests|FilterSortSelectTake", "B")]
+        public void GetDevExtreme(string t, JsonTestCase jsonTestCase) {
+            var ea = GetDevExtreme_ExpectedActual(t, jsonTestCase);
+            Assert.True(ea.Actual.IsEqualAndWrite(ea.Expected, Output));
+        }
 
 
-        //[Theory]
-        //[TestJson_("GetDevExtreme", "FilterSkipTake", "HueGt200Skip2Take5")]
-        //public void GetDevExtreme1(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var filter = jsonTestCase.GetObject<string>("Filter");
-        //    var skip = jsonTestCase.GetObject<int>("Skip");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-        //    var expected = jsonTestCase
-        //        .GetObject<ICollection<dynamic>>("Expected")
-        //        .OrderBy(x => x.Id)
-        //        .ToList();
+        [Theory]
+        [TestJson_("GetDynamicLinq", "ReadonlyControllerTests|WhereSkipTake", "A")] //RedGt200Skip2Take5
+        [TestJson_("GetDynamicLinq", "ReadonlyControllerTests|WhereOrderBySelectTake", "B")] //NameContainsBlueSelectNameDescSysUserTake10
+        public void GetDynamicLinq(string t, JsonTestCase jsonTestCase) {
+            var ea = GetDynamicLinq_ExpectedActual(t, jsonTestCase);
+            Assert.True(ea.Actual.IsEqualAndWrite(ea.Expected, Output));
+        }
 
 
-        //    var loadResult = HttpClient.Get<DeserializableLoadResult<dynamic>>($"api/hsl/devextreme?filter={filter}&skip={skip}&take={take}")
-        //        .GetObject<DeserializableLoadResult<dynamic>>();
-        //    var actual = loadResult.data
-        //        .OrderBy(x => x.Id)
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-        //}
-
-
-        //[Theory]
-        //[TestJson_("GetDevExtreme", "FilterSortSelectTake", "NameContainsBlueSelectNameDescSysUserTake10")]
-        //public void GetDevExtreme2(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var filter = jsonTestCase.GetObject<string>("Filter");
-        //    var select = jsonTestCase.GetObject<string>("Select");
-        //    var sort = jsonTestCase.GetObject<string>("Sort");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-        //    var expected = jsonTestCase
-        //        .GetObject<ICollection<dynamic>>("Expected")
-        //        .OrderByDescending(x => x.Name)
-        //        .ToList();
-
-        //    var loadResult = HttpClient.Get<DeserializableLoadResult<dynamic>>($"api/hsl/devextreme?filter={filter}&select={select}&sort={sort}&take={take}")
-        //        .GetObject<DeserializableLoadResult<dynamic>>();
-        //    var actual = loadResult.data
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-        //}
-
-        //[Theory]
-        //[TestJson_("GetDynamicLinq", "WhereSkipTake", "HueGt200Skip2Take5")]
-        //public void GetDynamicLinq1(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var where = jsonTestCase.GetObject<string>("Where");
-        //    var skip = jsonTestCase.GetObject<int>("Skip");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/linq?where={where}&skip={skip}&take={take}")
-        //        .GetObject<List<dynamic>>()
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-        //}
-
-        //[Theory]
-        //[TestJson_("GetDynamicLinq", "WhereOrderBySelectTake", "NameContainsBlueSelectNameDescSysUserTake10")]
-        //public void GetDynamicLinq2(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var where = jsonTestCase.GetObject<string>("Where");
-        //    var select = jsonTestCase.GetObject<string>("Select");
-        //    var orderBy = jsonTestCase.GetObject<string>("OrderBy");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderByDescending(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/linq?where={where}&select={select}&orderBy={orderBy}&take={take}")
-        //        .GetObject<List<dynamic>>()
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-
-        //}
-
-
-        //[Theory]
-        //[TestJson_("GetDynamicLinqAsync", "WhereSkipTake", "HueGt200Skip2Take5")]
-        //public void GetDynamicLinqAsync1(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var where = jsonTestCase.GetObject<string>("Where");
-        //    var skip = jsonTestCase.GetObject<int>("Skip");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/linq/async?where={where}&skip={skip}&take={take}")
-        //        .GetObject<List<dynamic>>()
-        //        .OrderBy(x => x.Name)
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-        //}
-
-        //[Theory]
-        //[TestJson_("GetDynamicLinqAsync", "WhereOrderBySelectTake", "NameContainsBlueSelectNameDescSysUserTake10")]
-        //public void GetDynamicLinqAsync2(string t, JsonTestCase jsonTestCase) {
-        //    Output.WriteLine(t);
-        //    Output.WriteLine($"Db instance name: {InstanceName}");
-
-        //    var where = jsonTestCase.GetObject<string>("Where");
-        //    var select = jsonTestCase.GetObject<string>("Select");
-        //    var orderBy = jsonTestCase.GetObject<string>("OrderBy");
-        //    var take = jsonTestCase.GetObject<int>("Take");
-
-        //    var expected = jsonTestCase.GetObject<List<dynamic>>("Expected")
-        //        .OrderByDescending(x => x.Name)
-        //        .ToList();
-
-        //    var actual = HttpClient.Get<List<dynamic>>($"api/hsl/linq/async?where={where}&select={select}&orderBy={orderBy}&take={take}")
-        //        .GetObject<List<dynamic>>()
-        //        .ToList();
-
-        //    Assert.True(actual.IsEqualOrWrite(expected, Output));
-
-        //}
+        [Theory]
+        [TestJson_("GetDynamicLinq", "ReadonlyControllerTests|WhereSkipTake", "A")] //RedGt200Skip2Take5
+        [TestJson_("GetDynamicLinq", "ReadonlyControllerTests|WhereOrderBySelectTake", "B")] //NameContainsBlueSelectNameDescSysUserTake10
+        public async Task GetDynamicLinqAsync(string t, JsonTestCase jsonTestCase) {
+            var ea = await GetDynamicLinqAsync_ExpectedActual(t, jsonTestCase);
+            Assert.True(ea.Actual.IsEqualAndWrite(ea.Expected, Output));
+        }
 
 
         //[Theory]
