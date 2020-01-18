5a88ac51cfc8508e183567904aa2e6a7ff874b47
Dennis Mitchell <den.mitchell@yahoo.com>
2020-01-18 05:40:16 -0500

    ongoing setup of sample solution

---
+++ sample/Hr.Launcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using I = IdentityServer.Lib;
+using A = Hr.Api.Lib;
+using R = Hr.RazorApp;
+using EDennis.AspNetCore.Base.Web;
+using System.Threading.Tasks;
+
+namespace Hr.Launcher {
+    public class Launcher : ILauncher {
+
+        public static void Main(string[] args) {
+            new Launcher().Launch(args);
+            LauncherUtils.Block();
+        }
+
+        public void Launch(string[] args) {
+            var is4 = new I.Program().Run(args);
+            var api = new A.Program().Run(args);
+            if(args[0].Contains("razor", StringComparison.OrdinalIgnoreCase))
+                Task.Run(() => R.Program.Main(args));
+            ProgramBase.CanPingAsync(is4, api);
+        }
+
+
+    }
+}
