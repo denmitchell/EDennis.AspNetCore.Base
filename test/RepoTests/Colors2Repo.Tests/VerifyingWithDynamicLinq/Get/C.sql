31e89c6f324a9291a82910b3724f2e308c9c276a
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2020-01-29 12:22:49 -0500

    Update test sql

--- test/RepoTests/Colors2Repo.Tests/VerifyingWithDynamicLinq/Get/C.sql
+++ test/RepoTests/Colors2Repo.Tests/VerifyingWithDynamicLinq/Get/C.sql
@@ -10,11 +10,12 @@ declare @TargetId int = -999299
 begin transaction
 
 declare @Expected varchar(max) = null
+declare @Exception varchar(max) = null
 
 rollback transaction
 --exec _.ResetSequences --only needed if no explicit Ids are provided
 
 exec _.SaveTestJson @ProjectName, @ClassName, @MethodName, @TestScenario, @TestCase, 'Id', @TargetId
 --exec _.SaveTestJson @ProjectName, @ClassName, @MethodName, @TestScenario, @TestCase, 'Expected', @Expected
-exec _.SaveTestJson @ProjectName, @ClassName, @MethodName, @TestScenario, @TestCase, 'Exception', @Exception
+--exec _.SaveTestJson @ProjectName, @ClassName, @MethodName, @TestScenario, @TestCase, 'Exception', @Exception
 exec _.GetTestJson @ProjectName, @ClassName, @MethodName, @TestScenario, @TestCase
