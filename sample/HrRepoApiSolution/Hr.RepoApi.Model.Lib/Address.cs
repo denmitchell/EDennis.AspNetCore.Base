920ecc41be014f386f3a03690429b6900590c033
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2020-02-05 17:03:05 -0500

    Added PersonId/Person to Address Model

--- sample/HrRepoApiSolution/Hr.RepoApi.Model.Lib/Address.cs
+++ sample/HrRepoApiSolution/Hr.RepoApi.Model.Lib/Address.cs
@@ -1,16 +1,38 @@
 ﻿using EDennis.AspNetCore.Base.EntityFramework;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hr.Api.Models {
     public class Address : IHasIntegerId, IHasSysUser {
+        [Key]
         public int Id { get; set; }
+
+        [StringLength(100)]
         public string StreetAddress { get; set; }
+
+        [StringLength(40)]
         public string City { get; set; }
+
+        [StringLength(2, MinimumLength =2)]
         public string State { get; set; }
+
+        [RegularExpression("[0-9]{5}")]
         public string PostalCode { get; set; }
+
+
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+        public int PersonId { get; set; }
+
+
+        [StringLength(100)]
         public string SysUser { get; set; }
+
+        [DataType("datetime2(7)")]
         public DateTime SysStart { get; set; }
+
+        [DataType("datetime2(7)")]
         public DateTime SysEnd { get; set; }
     }
 }
