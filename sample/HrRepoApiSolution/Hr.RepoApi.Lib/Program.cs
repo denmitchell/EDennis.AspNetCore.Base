2f13a04e94cd56b8a2b59fd3e01005d6790f54ee
Dennis Mitchell <den.mitchell@yahoo.com>
2020-02-15 17:08:24 -0500

    Update IdentityServer Program.cs and Hr.RepoApi Program.cs to use ConfigurationUtils (add ConfigurationType required property)

--- sample/HrRepoApiSolution/Hr.RepoApi.Lib/Program.cs
+++ sample/HrRepoApiSolution/Hr.RepoApi.Lib/Program.cs
@@ -1,7 +1,9 @@
+using EDennis.AspNetCore.Base;
 using EDennis.AspNetCore.Base.Web;
 
 namespace Hr.RepoApi.Lib {
     public class Program : ProgramBase<Startup> {
         public override string ProjectName => "Hr.RepoApi";
+        public override ConfigurationType ConfigurationType => ConfigurationType.ConfigurationApi;
     }
 }
